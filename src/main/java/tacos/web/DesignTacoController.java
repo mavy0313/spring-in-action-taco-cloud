797788efb8b42b9785f1db35ed7197bdaa514a35
Maksim Vypov <maksim_vypov@epam.com>
2019-07-18 11:15:50 +0400

    Added validation

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,10 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
@@ -19,8 +22,8 @@ import tacos.Taco;
 @RequestMapping("/design")
 public class DesignTacoController {
 
-  @GetMapping
-  public String showDesignForm(Model model) {
+  @ModelAttribute
+  public void addIngredientsToModel(Model model) {
     List<Ingredient> ingredients = Arrays.asList(
         new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
         new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
@@ -33,20 +36,28 @@ public class DesignTacoController {
         new Ingredient("SLSA", "Salsa", Type.SAUCE),
         new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
     );
+
     Type[] types = Ingredient.Type.values();
     for (Type type : types) {
       model.addAttribute(type.toString().toLowerCase(),
           filterByType(ingredients, type));
     }
+  }
+
+  @GetMapping
+  public String showDesignForm(Model model) {
     model.addAttribute("design", new Taco());
     return "design";
   }
 
   @PostMapping
-  public String processDesign(Taco taco) {
+  public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+    if (errors.hasErrors()) {
+      return "design";
+    }
     // Save the taco design...
     // We'll do this in chapter 3
-    log.info("Processing design: " + taco);
+    log.info("Processing design: " + design);
     return "redirect:/orders/current";
   }
 
