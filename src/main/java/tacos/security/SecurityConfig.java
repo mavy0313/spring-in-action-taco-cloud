4119ea16a2cb845e3a7ab94e1a8fa1302cbb753b
mavy0313 <vypoff@gmail.com>
2020-04-26 21:06:44 +0400

    Login logout

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -35,9 +35,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http
         .authorizeRequests()
           .antMatchers("/design", "/orders")
-            .hasRole("ROLE_USER")
+            .access("hasRole('ROLE_USER')")
           .antMatchers("/", "/**")
-            .permitAll();
+            .access("permitAll")
+          .and()
+            .formLogin()
+            .loginPage("/login")
+            .defaultSuccessUrl("/design", true)
+          .and()
+            .logout()
+            .logoutSuccessUrl("/");
   }
 
 //  @Override
