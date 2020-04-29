16a1a61c243698f7a9ef364639dc514ed914afc4
mavy0313 <vypoff@gmail.com>
2020-04-29 10:47:33 +0400

    Security completed

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,8 +2,8 @@ package tacos.web;
 
 import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.Order;
+import tacos.User;
 import tacos.data.OrderRepository;
 
 @Slf4j
@@ -31,10 +32,15 @@ public class OrderController {
   }
 
   @PostMapping
-  public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+  public String processOrder(
+      @Valid Order order,
+      Errors errors,
+      SessionStatus sessionStatus,
+      @AuthenticationPrincipal User user) {
     if (errors.hasErrors()) {
       return "orderForm";
     }
+    order.setUser(user);
     orderRepo.save(order);
     sessionStatus.setComplete();
     return "redirect:/";
